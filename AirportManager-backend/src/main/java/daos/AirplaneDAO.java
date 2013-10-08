846546a6113d4da9775bc70c9cc704ae92cd44b1
halafi <xgam33@gmail.com>
2013-10-08 20:14:14 +0200

    AirplaneDAO

--- AirportManager-backend/src/main/java/daos/AirplaneDAO.java
+++ AirportManager-backend/src/main/java/daos/AirplaneDAO.java
@@ -9,11 +9,48 @@ import java.util.List;
  * @author Filip
  */
 public interface AirplaneDAO {
+    /**
+     * Creates new airplane.
+     * 
+     * @param airplane Airplane to be created 
+     */
     public void createAirplane(Airplane airplane);
+    
+    /**
+     * Updates an existing airplane.
+     * 
+     * @param airplane
+     */
     public void updateAirplane(Airplane airplane);
+    
+    /**
+     * Removes an existing airplane.
+     * 
+     * @param airplane 
+     */
     public void removeAirplane(Airplane airplane);
+    
+    /**
+     * Returns Airplane with desired id.
+     * 
+     * @param id id of desired Airplane
+     * @return desired Airplane1
+     */
     public Airplane getAirplane(Long id);
+    
+    /**
+     * Returns all airplanes.
+     * 
+     * @return List of all airplanes.
+     */
     public List<Airplane> getAllAirplanes();
+    
+    /**
+     * Returns all airplanes for a flight.
+     * 
+     * @param airplane desired airplane
+     * @return list of desired airplane flights
+     */
     public List<Flight> getAllAirplanesFlights(Airplane airplane);
     
 }
