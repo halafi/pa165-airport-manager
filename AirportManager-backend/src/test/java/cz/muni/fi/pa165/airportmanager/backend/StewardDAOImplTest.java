1678c70848804738ab679094f932fd78260c5949
halafi <xgam33@gmail.com>
2013-10-09 16:12:54 +0200

    StewardDAOImplTest in progress, POM - different JUnit version dependency

--- AirportManager-backend/src/test/java/cz/muni/fi/pa165/airportmanager/backend/StewardDAOImplTest.java
+++ AirportManager-backend/src/test/java/cz/muni/fi/pa165/airportmanager/backend/StewardDAOImplTest.java
@@ -1,14 +1,20 @@
 package cz.muni.fi.pa165.airportmanager.backend;
 
+import cz.muni.fi.pa165.airportmanager.backend.daos.StewardDAO;
 import junit.framework.TestCase;
+import org.junit.Before;
 
 /**
+ * Tests for StewardDAOImpl.
  *
  * @author Filip
  */
 public class StewardDAOImplTest extends TestCase {
+    private StewardDAO stewardDAO;
     
-    public StewardDAOImplTest(String testName) {
-        super(testName);
+    @Before
+    public void setUp() {
+        
     }
+
 }
