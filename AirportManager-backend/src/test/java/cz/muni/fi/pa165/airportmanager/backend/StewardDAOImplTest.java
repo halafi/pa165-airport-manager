b851b856321d1bb17a502dc6c5cde7a84c91ec2c
halafi <xgam33@gmail.com>
2013-10-09 15:56:40 +0200

    FlightDAOImpl (JPA), empty StewardDAOImplTest

---
+++ AirportManager-backend/src/test/java/cz/muni/fi/pa165/airportmanager/backend/StewardDAOImplTest.java
@@ -0,0 +1,14 @@
+package cz.muni.fi.pa165.airportmanager.backend;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Filip
+ */
+public class StewardDAOImplTest extends TestCase {
+    
+    public StewardDAOImplTest(String testName) {
+        super(testName);
+    }
+}
