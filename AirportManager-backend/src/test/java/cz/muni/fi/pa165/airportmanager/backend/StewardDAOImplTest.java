c63603d982aeeee0b873fb7b90078b0f344dbf71
halafi <xgam33@gmail.com>
2013-10-09 18:37:34 +0200

    StewardDAOImplTest

--- AirportManager-backend/src/test/java/cz/muni/fi/pa165/airportmanager/backend/StewardDAOImplTest.java
+++ AirportManager-backend/src/test/java/cz/muni/fi/pa165/airportmanager/backend/StewardDAOImplTest.java
@@ -1,10 +1,16 @@
 package cz.muni.fi.pa165.airportmanager.backend;
 
 import cz.muni.fi.pa165.airportmanager.backend.daos.StewardDAO;
+import cz.muni.fi.pa165.airportmanager.backend.entities.Airplane;
+import cz.muni.fi.pa165.airportmanager.backend.entities.Destination;
+import cz.muni.fi.pa165.airportmanager.backend.entities.Flight;
 import cz.muni.fi.pa165.airportmanager.backend.entities.Steward;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import static junit.framework.Assert.assertNotNull;
 import junit.framework.TestCase;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
@@ -23,14 +29,132 @@ public class StewardDAOImplTest extends TestCase {
         stewardDAO = new StewardDAOImpl();
     }
     
+    /**
+     * Constructor for Steward.
+     */
+    private static Steward newSteward(String firstName, String lastName) {
+        Steward steward = new Steward();
+        steward.setFirstName(firstName);
+        steward.setLastName(lastName);
+        return steward;
+    }
+    
+    /**
+     * Constructor for Flight.
+     */
+    private static Flight newFlight(Timestamp departureTime, 
+            Timestamp arrivalTime, Destination origin, Destination target,
+            Airplane airplane, List<Steward> stewardList) {
+        Flight flight = new Flight();
+        flight.setDepartureTime(departureTime);
+        flight.setArrivalTime(arrivalTime);
+        flight.setOrigin(origin);
+        flight.setTarget(target);
+        flight.setAirplane(airplane);
+        flight.setStewardList(stewardList);
+        return flight;
+    }
+    
+    /**
+     * Constructor for Destination.
+     */
+    private static Destination newDestination(String code, String country, 
+            String city) {
+        Destination destination = new Destination();
+        destination.setCode(code);
+        destination.setCountry(country);
+        destination.setCity(city);
+        return destination;
+    }
+    
+    /**
+     * Constructor for Airplane.
+     */
+    private static Airplane newAirplane(int capacity, String name, String type) {
+        Airplane plane = new Airplane();
+        plane.setCapacity(capacity);
+        plane.setName(name);
+        plane.setType(type);
+        return plane;
+    }
+    
+    /**
+     * Test for get steward.
+     */
     @Test
+    public void getSteward(){
+        Steward steward1 = newSteward("Elaine","Dickinson");
+        Steward steward2 = newSteward("Joshua","Bloch");
+        stewardDAO.createSteward(steward1);
+        stewardDAO.createSteward(steward2);
+        
+        assertDeepEquals(stewardDAO.getSteward(steward1.getId()),steward1);
+        assertDeepEquals(stewardDAO.getSteward(steward2.getId()),steward2);
+    }
+    
+    /**
+     * Test for getting steward with null id.
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void getStewardWithNullId(){
+        stewardDAO.getSteward(null);
+    }
+    
+    /**
+     * Test for getting all stewards.
+     */
+    @Test
+    public void getAllStewards() {
+        assertTrue(stewardDAO.getAllStewards().isEmpty());
+        
+        Steward steward1 = newSteward("Elaine","Dickinson");
+        Steward steward2 = newSteward("Joshua","Bloch");
+        stewardDAO.createSteward(steward1);
+        stewardDAO.createSteward(steward2);
+
+        List<Steward> expected = Arrays.asList(steward1,steward2);
+        List<Steward> actual = stewardDAO.getAllStewards();
+
+        Collections.sort(actual,idComparator);
+        Collections.sort(expected,idComparator);
+
+        assertEquals(expected, actual);
+        assertDeepEquals(expected, actual);
+    }
+    
+    /**
+     * Test for getting all stewards flights.
+     */
+    @Test
+    public void getAllStewardsFlights() {
+        Airplane plane1 = newAirplane(700,"Jet3000","Passenger transport");
+        Destination dest1 = newDestination("CZB","Czech Republic","Brno");
+        Destination dest2 = newDestination("USN","United States","New York");
+        Steward steward1 = newSteward("Elaine","Dickinson");
+        stewardDAO.createSteward(steward1);
+        
+        assertTrue(stewardDAO.getAllStewardsFlights(steward1).isEmpty());
+        
+        List<Flight> expected = Collections.EMPTY_LIST;
+        List<Flight> actual = stewardDAO.getAllStewardsFlights(steward1);
+        assertEquals(expected, actual);
+        
+        List<Steward> stewList = new ArrayList<Steward>();
+        stewList.add(steward1);
+        
+        Flight flight1 = newFlight(new Timestamp(100000),new Timestamp(500000),dest1,dest2,plane1,stewList);
+        List<Flight> flightList = new ArrayList<Flight>();
+        flightList.add(flight1);
+        
+        assertEquals(stewardDAO.getAllStewardsFlights(steward1),flightList);
+    }
+    
     /**
      * Test for steward creation.
      */
+    @Test
     public void createSteward() {
-        Steward steward = new Steward();
-        steward.setFirstName("Elaine");
-        steward.setLastName("Dickinson");
+        Steward steward = newSteward("Elaine","Dickinson");
         stewardDAO.createSteward(steward);
         
         assertNotNull(steward.getId());
@@ -43,6 +167,7 @@ public class StewardDAOImplTest extends TestCase {
         assertNotSame(steward, sameSteward);
         assertDeepEquals(steward, sameSteward);
     }
+    
     /**
      * Attempt of creating null steward.
      */
@@ -51,6 +176,7 @@ public class StewardDAOImplTest extends TestCase {
         Steward steward = null;
         stewardDAO.createSteward(steward);
     }
+    
     /**
      * Attempt of creating steward without last name.
      */
@@ -60,6 +186,7 @@ public class StewardDAOImplTest extends TestCase {
         steward.setFirstName("Elaine");
         stewardDAO.createSteward(steward);
     }
+    
     /**
      * Attempt of creating steward without first name.
      */
@@ -69,9 +196,44 @@ public class StewardDAOImplTest extends TestCase {
         steward.setLastName("Dickinson");
         stewardDAO.createSteward(steward);
     }
+    
+    @Test
+    /**
+     * Test for steward updating.
+     */
+    public void updateSteward() {
+        Steward steward = new Steward();
+        steward.setFirstName("Elaine");
+        steward.setLastName("Dickinson");
+        stewardDAO.createSteward(steward);
+        
+        Steward steward2 = stewardDAO.getSteward(steward.getId());
+        steward2.setFirstName("Jane");
+        stewardDAO.updateSteward(steward2);
+        Steward steward3 = stewardDAO.getSteward(steward.getId());
+        
+        assertEquals(steward2, steward3);
+        assertNotSame(steward2, steward3);
+    }
+    
+    @Test
+    /**
+     * Test for removing steward.
+     */
+    public void removeSteward(){
+        Steward steward1 = newSteward("Elaine","Dickinson");
+        Steward steward2 = newSteward("Joshua","Bloch");
+        stewardDAO.createSteward(steward1);
+        stewardDAO.createSteward(steward2);
 
-    
-    
+        assertNotNull(stewardDAO.getSteward(steward1.getId()));
+        assertNotNull(stewardDAO.getSteward(steward2.getId()));
+
+        stewardDAO.removeSteward(steward1);
+        
+        assertNull(stewardDAO.getSteward(steward1.getId()));
+        assertNotNull(stewardDAO.getSteward(steward2.getId()));
+    }
     
     /**
      * Check for equality of all steward parameters.
@@ -96,7 +258,7 @@ public class StewardDAOImplTest extends TestCase {
         }
     }
     /**
-     * Comparatord by id.
+     * Comparator by id.
      */
     private static Comparator<Steward> idComparator = new Comparator<Steward>() {
         @Override
