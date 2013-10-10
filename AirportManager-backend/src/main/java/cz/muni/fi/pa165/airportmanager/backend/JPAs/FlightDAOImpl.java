68f55a6ed31064acf85d7d04d6c9cda805610ead
halafi <xgam33@gmail.com>
2013-10-10 19:54:44 +0200

    FlightDAOImpl, StewardDAOImplTest, persistence.xml should be set properly

--- AirportManager-backend/src/main/java/cz/muni/fi/pa165/airportmanager/backend/JPAs/FlightDAOImpl.java
+++ AirportManager-backend/src/main/java/cz/muni/fi/pa165/airportmanager/backend/JPAs/FlightDAOImpl.java
@@ -4,7 +4,7 @@ import cz.muni.fi.pa165.airportmanager.backend.daos.FlightDAO;
 import cz.muni.fi.pa165.airportmanager.backend.entities.Flight;
 import java.util.List;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
 
 /**
@@ -14,8 +14,11 @@ import javax.persistence.Query;
  */
 public class FlightDAOImpl implements FlightDAO {
     
-    @PersistenceContext
-    private EntityManager em;
+    private EntityManagerFactory emf;
+    
+    public void setEntityManagerFactory(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
     
     public void createFlight(Flight flight) throws IllegalArgumentException {
         if(flight == null) {
@@ -31,7 +34,11 @@ public class FlightDAOImpl implements FlightDAO {
         } else if(flight.getAirplane() == null) {
             throw new IllegalArgumentException("Airplane is null.");
         } else {
+            EntityManager em = emf.createEntityManager();
+            em.getTransaction().begin();
             em.persist(flight);
+            em.getTransaction().commit();
+            em.close();
         }
     }
     
@@ -48,10 +55,15 @@ public class FlightDAOImpl implements FlightDAO {
             throw new IllegalArgumentException("Origin or target destination is null.");
         } else if(flight.getAirplane() == null) {
             throw new IllegalArgumentException("Airplane is null.");
-        } else if(em.find(Flight.class, flight.getId()) == null){
-            throw new JPAException("Flight in database is null.");
         } else {
+            EntityManager em = emf.createEntityManager();
+            if(em.find(Flight.class, flight.getId()) == null) {
+                throw new JPAException("Flight in database is null.");
+            }
+            em.getTransaction().begin();
             em.merge(flight);
+            em.getTransaction().commit();
+            em.close();
         }
     }
     
@@ -61,11 +73,15 @@ public class FlightDAOImpl implements FlightDAO {
         } else if(flight.getId() == null) {
             throw new IllegalArgumentException("Flight flight to be removed id is not assigned.");
         } else {
+            EntityManager em = emf.createEntityManager();
             Flight flightToBeDeleted = em.find(Flight.class, flight.getId());
             if (flightToBeDeleted == null) {
                 throw new JPAException("Flight in database is null.");
             }
+            em.getTransaction().begin();
             em.remove(flightToBeDeleted);
+            em.getTransaction().commit();
+            em.close();
         }
     }
     
@@ -73,15 +89,20 @@ public class FlightDAOImpl implements FlightDAO {
         if(id == null) {
             throw new IllegalArgumentException("Id is null.");
         }
-        if(em.find(Flight.class, id) == null) {
+        EntityManager em = emf.createEntityManager();
+        Flight toReturn = em.find(Flight.class, id);
+        if(toReturn == null) {
             throw new JPAException("Flight is not in database.");
         }
-        return em.find(Flight.class, id);
+        em.close();
+        return toReturn;
     }
     
     public List<Flight> getAllFlight(){
+        EntityManager em = emf.createEntityManager();
         Query query = em.createQuery("SELECT p FROM Flight p ");
         List<Flight> allFlights = query.getResultList();
+        em.close();
         return allFlights;
     }
 }
--- AirportManager-backend/src/test/java/cz/muni/fi/pa165/airportmanager/backend/StewardDAOImplTest.java
+++ AirportManager-backend/src/test/java/cz/muni/fi/pa165/airportmanager/backend/StewardDAOImplTest.java
@@ -1,7 +1,13 @@
 package cz.muni.fi.pa165.airportmanager.backend;
 
+import cz.muni.fi.pa165.airportmanager.backend.JPAs.AirplaneDAOImpl;
+import cz.muni.fi.pa165.airportmanager.backend.JPAs.DestinationDAOImpl;
+import cz.muni.fi.pa165.airportmanager.backend.JPAs.FlightDAOImpl;
 import cz.muni.fi.pa165.airportmanager.backend.JPAs.JPAException;
 import cz.muni.fi.pa165.airportmanager.backend.JPAs.StewardDAOImpl;
+import cz.muni.fi.pa165.airportmanager.backend.daos.AirplaneDAO;
+import cz.muni.fi.pa165.airportmanager.backend.daos.DestinationDAO;
+import cz.muni.fi.pa165.airportmanager.backend.daos.FlightDAO;
 import cz.muni.fi.pa165.airportmanager.backend.daos.StewardDAO;
 import cz.muni.fi.pa165.airportmanager.backend.entities.Airplane;
 import cz.muni.fi.pa165.airportmanager.backend.entities.Destination;
@@ -18,26 +24,198 @@ import java.util.List;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import junit.framework.TestCase;
+import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
 /**
  * Tests for StewardDAOImpl.
  *
  * @author Filip
  */
-@RunWith(JUnit4.class)
 public class StewardDAOImplTest extends TestCase {
-    private StewardDAO stewardDAO;
+    private StewardDAO stewDAO;
+    private DestinationDAO destDAO;
+    private AirplaneDAO airplaneDAO;
+    private FlightDAO flightDAO;
     
+    /**
+     * Sets up the whole test thing.
+     * @throws SQLException
+     */
     @Before
     public void setUp() throws SQLException {
-        stewardDAO = new StewardDAOImpl();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("AirportManagerTest");
+        stewDAO = new StewardDAOImpl();
+        //stewDAO.setEntityManagerFactory(emf);
+        //destDAO = new DestinationDAOImpl();
+        //destDAO.setEntityManagerFactory(emf);
+        //airplaneDAO = new AirplaneDAOImpl();
+        //airDAO.setEntityManagerFactory(emf);
+        //flightDAO = new FlightDAOImpl();
+        //flightDAO.setEntityManagerFactory(emf);
+    }
+
+    /**
+     * Test for get steward.
+     */
+    @Test
+    public void testGetSteward() throws JPAException{
+        Steward steward1 = newSteward("Elaine","Dickinson");
+        Steward steward2 = newSteward("Joshua","Bloch");
+        stewDAO.createSteward(steward1);
+        stewDAO.createSteward(steward2);
         
+        assertDeepEquals(stewDAO.getSteward(steward1.getId()),steward1);
+        assertDeepEquals(stewDAO.getSteward(steward2.getId()),steward2);
+    }
+    
+    /**
+     * Test for getting steward with null id.
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testGetStewardWithNullId() throws JPAException{
+        stewDAO.getSteward(null);
+    }
+    
+    /**
+     * Test for getting all stewards.
+     */
+    @Test
+    public void testGetAllStewards() throws JPAException {
+        assertTrue(stewDAO.getAllStewards().isEmpty());
         
+        Steward steward1 = newSteward("Elaine","Dickinson");
+        Steward steward2 = newSteward("Joshua","Bloch");
+        stewDAO.createSteward(steward1);
+        stewDAO.createSteward(steward2);
+
+        List<Steward> expected = Arrays.asList(steward1,steward2);
+        List<Steward> actual = stewDAO.getAllStewards();
+
+        Collections.sort(actual,idComparator);
+        Collections.sort(expected,idComparator);
+
+        assertEquals(expected, actual);
+        assertDeepEquals(expected, actual);
+    }
+    
+    /**
+     * Test for getting all stewards flights.
+     */
+    @Test
+    public void testGetAllStewardsFlights() throws JPAException {
+        Airplane plane1 = newAirplane(700,"Jet3000","Passenger transport");
+        airplaneDAO.createAirplane(plane1);
+        Destination dest1 = newDestination("CZB","Czech Republic","Brno");
+        destDAO.createDestination(dest1);
+        Destination dest2 = newDestination("USN","United States","New York");
+        destDAO.createDestination(dest2);
+        Steward steward1 = newSteward("Elaine","Dickinson");
+        stewDAO.createSteward(steward1);
+        
+        assertTrue(stewDAO.getAllStewardsFlights(steward1).isEmpty());
+        
+        List<Flight> expected = Collections.EMPTY_LIST;
+        List<Flight> actual = stewDAO.getAllStewardsFlights(steward1);
+        assertEquals(expected, actual);
+        
+        List<Steward> stewList = new ArrayList<Steward>();
+        stewList.add(steward1);
+        
+        Flight flight1 = newFlight(new Timestamp(100000),new Timestamp(500000),dest1,dest2,plane1,stewList);
+        flightDAO.createFlight(flight1);
+        List<Flight> flightList = new ArrayList<Flight>();
+        flightList.add(flight1);
+        
+        assertEquals(stewDAO.getAllStewardsFlights(steward1),flightList);
+    }
+    
+    /**
+     * Test for steward creation.
+     */
+    @Test
+    public void testCreateSteward() throws JPAException {
+        Steward steward = newSteward("Elaine","Dickinson");
+        stewDAO.createSteward(steward);
+        
+        assertNotNull(steward.getId());
+        assertNotNull(steward.getFirstName());
+        assertNotNull(steward.getLastName());
+        
+        Steward sameSteward = stewDAO.getSteward(steward.getId());
+        
+        assertEquals(steward, sameSteward);
+        assertNotSame(steward, sameSteward);
+        assertDeepEquals(steward, sameSteward);
+    }
+    
+    /**
+     * Attempt of creating null steward.
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testCreateNullSteward() throws JPAException {
+        Steward steward = null;
+        stewDAO.createSteward(steward);
+    }
+    
+    /**
+     * Attempt of creating steward without last name.
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testCreateStewardWithoutLastName() throws JPAException {
+        Steward steward = new Steward();
+        steward.setFirstName("Elaine");
+        stewDAO.createSteward(steward);
+    }
+    
+    /**
+     * Attempt of creating steward without first name.
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testCreateStewardWithoutFirstName() throws JPAException {
+        Steward steward = new Steward();
+        steward.setLastName("Dickinson");
+        stewDAO.createSteward(steward);
+    }
+    
+    @Test
+    /**
+     * Test for steward updating.
+     */
+    public void testUpdateSteward() throws JPAException {
+        Steward steward = new Steward();
+        steward.setFirstName("Elaine");
+        steward.setLastName("Dickinson");
+        stewDAO.createSteward(steward);
+        
+        Steward steward2 = stewDAO.getSteward(steward.getId());
+        steward2.setFirstName("Jane");
+        stewDAO.updateSteward(steward2);
+        Steward steward3 = stewDAO.getSteward(steward.getId());
+        
+        assertEquals(steward2, steward3);
+        assertNotSame(steward2, steward3);
+    }
+    
+    @Test
+    /**
+     * Test for removing steward.
+     */
+    public void testRemoveSteward() throws JPAException{
+        Steward steward1 = newSteward("Elaine","Dickinson");
+        Steward steward2 = newSteward("Joshua","Bloch");
+        stewDAO.createSteward(steward1);
+        stewDAO.createSteward(steward2);
+
+        assertNotNull(stewDAO.getSteward(steward1.getId()));
+        assertNotNull(stewDAO.getSteward(steward2.getId()));
+
+        stewDAO.removeSteward(steward1);
+        
+        assertNull(stewDAO.getSteward(steward1.getId()));
+        assertNotNull(stewDAO.getSteward(steward2.getId()));
     }
     
     /**
@@ -87,163 +265,6 @@ public class StewardDAOImplTest extends TestCase {
         plane.setName(name);
         plane.setType(type);
         return plane;
-    }
-    
-    /**
-     * Test for get steward.
-     */
-    @Test
-    public void getSteward() throws JPAException{
-        Steward steward1 = newSteward("Elaine","Dickinson");
-        Steward steward2 = newSteward("Joshua","Bloch");
-        stewardDAO.createSteward(steward1);
-        stewardDAO.createSteward(steward2);
-        
-        assertDeepEquals(stewardDAO.getSteward(steward1.getId()),steward1);
-        assertDeepEquals(stewardDAO.getSteward(steward2.getId()),steward2);
-    }
-    
-    /**
-     * Test for getting steward with null id.
-     */
-    @Test (expected = IllegalArgumentException.class)
-    public void getStewardWithNullId() throws JPAException{
-        stewardDAO.getSteward(null);
-    }
-    
-    /**
-     * Test for getting all stewards.
-     */
-    @Test
-    public void getAllStewards() throws JPAException {
-        assertTrue(stewardDAO.getAllStewards().isEmpty());
-        
-        Steward steward1 = newSteward("Elaine","Dickinson");
-        Steward steward2 = newSteward("Joshua","Bloch");
-        stewardDAO.createSteward(steward1);
-        stewardDAO.createSteward(steward2);
-
-        List<Steward> expected = Arrays.asList(steward1,steward2);
-        List<Steward> actual = stewardDAO.getAllStewards();
-
-        Collections.sort(actual,idComparator);
-        Collections.sort(expected,idComparator);
-
-        assertEquals(expected, actual);
-        assertDeepEquals(expected, actual);
-    }
-    
-    /**
-     * Test for getting all stewards flights.
-     */
-    @Test
-    public void getAllStewardsFlights() throws JPAException {
-        Airplane plane1 = newAirplane(700,"Jet3000","Passenger transport");
-        Destination dest1 = newDestination("CZB","Czech Republic","Brno");
-        Destination dest2 = newDestination("USN","United States","New York");
-        Steward steward1 = newSteward("Elaine","Dickinson");
-        stewardDAO.createSteward(steward1);
-        
-        assertTrue(stewardDAO.getAllStewardsFlights(steward1).isEmpty());
-        
-        List<Flight> expected = Collections.EMPTY_LIST;
-        List<Flight> actual = stewardDAO.getAllStewardsFlights(steward1);
-        assertEquals(expected, actual);
-        
-        List<Steward> stewList = new ArrayList<Steward>();
-        stewList.add(steward1);
-        
-        Flight flight1 = newFlight(new Timestamp(100000),new Timestamp(500000),dest1,dest2,plane1,stewList);
-        List<Flight> flightList = new ArrayList<Flight>();
-        flightList.add(flight1);
-        
-        assertEquals(stewardDAO.getAllStewardsFlights(steward1),flightList);
-    }
-    
-    /**
-     * Test for steward creation.
-     */
-    @Test
-    public void createSteward() throws JPAException {
-        Steward steward = newSteward("Elaine","Dickinson");
-        stewardDAO.createSteward(steward);
-        
-        assertNotNull(steward.getId());
-        assertNotNull(steward.getFirstName());
-        assertNotNull(steward.getLastName());
-        
-        Steward sameSteward = stewardDAO.getSteward(steward.getId());
-        
-        assertEquals(steward, sameSteward);
-        assertNotSame(steward, sameSteward);
-        assertDeepEquals(steward, sameSteward);
-    }
-    
-    /**
-     * Attempt of creating null steward.
-     */
-    @Test (expected = IllegalArgumentException.class)
-    public void createNullSteward() throws JPAException {
-        Steward steward = null;
-        stewardDAO.createSteward(steward);
-    }
-    
-    /**
-     * Attempt of creating steward without last name.
-     */
-    @Test (expected = IllegalArgumentException.class)
-    public void createStewardWithoutLastName() throws JPAException {
-        Steward steward = new Steward();
-        steward.setFirstName("Elaine");
-        stewardDAO.createSteward(steward);
-    }
-    
-    /**
-     * Attempt of creating steward without first name.
-     */
-    @Test (expected = IllegalArgumentException.class)
-    public void createStewardWithoutFirstName() throws JPAException {
-        Steward steward = new Steward();
-        steward.setLastName("Dickinson");
-        stewardDAO.createSteward(steward);
-    }
-    
-    @Test
-    /**
-     * Test for steward updating.
-     */
-    public void updateSteward() throws JPAException {
-        Steward steward = new Steward();
-        steward.setFirstName("Elaine");
-        steward.setLastName("Dickinson");
-        stewardDAO.createSteward(steward);
-        
-        Steward steward2 = stewardDAO.getSteward(steward.getId());
-        steward2.setFirstName("Jane");
-        stewardDAO.updateSteward(steward2);
-        Steward steward3 = stewardDAO.getSteward(steward.getId());
-        
-        assertEquals(steward2, steward3);
-        assertNotSame(steward2, steward3);
-    }
-    
-    @Test
-    /**
-     * Test for removing steward.
-     */
-    public void removeSteward() throws JPAException{
-        Steward steward1 = newSteward("Elaine","Dickinson");
-        Steward steward2 = newSteward("Joshua","Bloch");
-        stewardDAO.createSteward(steward1);
-        stewardDAO.createSteward(steward2);
-
-        assertNotNull(stewardDAO.getSteward(steward1.getId()));
-        assertNotNull(stewardDAO.getSteward(steward2.getId()));
-
-        stewardDAO.removeSteward(steward1);
-        
-        assertNull(stewardDAO.getSteward(steward1.getId()));
-        assertNotNull(stewardDAO.getSteward(steward2.getId()));
     }
     
     /**
