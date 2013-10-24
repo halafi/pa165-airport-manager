c89fbc1a21561d3bb92e01e4bd7d9dc53909a626
chorke <jura.durani@gamil.com>
2013-10-24 14:39:58 +0200

    DestinationServiceTest

---
+++ AirportManager/AirportManager-backend/src/test/java/cz/muni/fi/pa165/airportmanager/backend/services/DestinationServiceImplTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.airportmanager.backend.services;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Juraj Duráni
+ */
+public class DestinationServiceImplTest {
+    
+    
+    @Test
+    public void createTest(){
+        //TODO
+    }
+    
+    @Test
+    public void removeTest(){
+        //TODO
+    }
+    
+    @Test
+    public void updateTest(){
+        //TODO
+    }
+    
+    @Test
+    public void getTest(){
+        //TODO
+    }
+    
+    @Test
+    public void getAllTest(){
+        //TODO
+    }
+    
+    
+}
