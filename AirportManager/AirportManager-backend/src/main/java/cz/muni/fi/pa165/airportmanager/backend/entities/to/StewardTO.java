59b22f3e1ab8a0cd8db9e818a6253bda2e678707
halafi <xgam33@gmail.com>
2013-10-30 08:46:46 +0100

    StewardService, StewardTO

---
+++ AirportManager/AirportManager-backend/src/main/java/cz/muni/fi/pa165/airportmanager/backend/entities/to/StewardTO.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.muni.fi.pa165.airportmanager.backend.entities.to;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Filip
+ */
+public class StewardTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    @Override
+    public String toString() {
+        return "StewardTO{" + "id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final StewardTO other = (StewardTO) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+}
