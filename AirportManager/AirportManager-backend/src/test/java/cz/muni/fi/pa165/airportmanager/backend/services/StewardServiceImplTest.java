6c243b08b55535871a4e3ae023f8ddf190b3bd0c
halafi <xgam33@gmail.com>
2013-10-31 17:04:56 +0100

    AbstractServiceTest, StewardServiceImplTest - skeleton

--- AirportManager/AirportManager-backend/src/test/java/cz/muni/fi/pa165/airportmanager/backend/services/StewardServiceImplTest.java
+++ AirportManager/AirportManager-backend/src/test/java/cz/muni/fi/pa165/airportmanager/backend/services/StewardServiceImplTest.java
@@ -1,11 +1,47 @@
 
 package cz.muni.fi.pa165.airportmanager.backend.services;
 
+import cz.muni.fi.pa165.airportmanager.backend.AbstractServiceTest;
+import cz.muni.fi.pa165.airportmanager.backend.daos.StewardDAO;
 import org.junit.Test;
+import org.mockito.Mock;
 /**
  *
  * @author Filip
  */
-public class StewardServiceImplTest {
+public class StewardServiceImplTest extends AbstractServiceTest {
+    
+    @Mock
+    private StewardDAO stewDAO;
+    
+    @Test
+    public void testCreateSteward() {
+        
+    }
+    
+    @Test
+    public void testUpdateSteward() {
+        
+    }
+    
+    @Test
+    public void testRemoveSteward() {
+        
+    }
+    
+    @Test
+    public void testFindSteward() {
+        
+    }
+    
+    @Test
+    public void testFindAllStewards() {
+        
+    }
+    
+    @Test
+    public void testGetAllStewardsFlights() {
+        
+    }
     
 }
