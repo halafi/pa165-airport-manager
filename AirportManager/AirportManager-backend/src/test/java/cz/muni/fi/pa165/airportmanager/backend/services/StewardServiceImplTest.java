12893b1182d2fed782d8091dbe14fb0f7347a892
halafi <xgam33@gmail.com>
2013-11-01 11:49:59 +0100

    StewardServiceImplTest - update

--- AirportManager/AirportManager-backend/src/test/java/cz/muni/fi/pa165/airportmanager/backend/services/StewardServiceImplTest.java
+++ AirportManager/AirportManager-backend/src/test/java/cz/muni/fi/pa165/airportmanager/backend/services/StewardServiceImplTest.java
@@ -4,15 +4,23 @@ import cz.muni.fi.pa165.airportmanager.backend.AbstractServiceTest;
 import cz.muni.fi.pa165.airportmanager.backend.daos.impl.JPAException;
 import cz.muni.fi.pa165.airportmanager.backend.services.impl.StewardServiceImpl;
 import cz.muni.fi.pa165.airportmanager.backend.daos.StewardDAO;
+import cz.muni.fi.pa165.airportmanager.backend.daos.impl.StewardDAOImpl;
 import cz.muni.fi.pa165.airportmanager.backend.entities.to.EntityDTOTransformer;
 import cz.muni.fi.pa165.airportmanager.backend.entities.to.StewardTO;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
+import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import static org.mockito.Mockito.verify;
+import org.mockito.MockitoAnnotations;
 import org.springframework.dao.DataAccessException;
 /**
  *
@@ -25,14 +33,34 @@ public class StewardServiceImplTest extends AbstractServiceTest {
     @InjectMocks
     private StewardServiceImpl service;
 
+    @BeforeClass
+    public static void beforeClass() {
+       
+    }
+    
+    @Before
+     public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @After
+    public void reset() {
+        Mockito.reset(stewDAO);
+        
+    }
     /**
      * Attempts to create Null Steward. Good luck with that!
+     * @throws cz.muni.fi.pa165.airportmanager.backend.daos.impl.JPAException
      */
     @Test (expected=DataAccessException.class)
     public void testCreateNull() throws JPAException {
         service.createSteward(null);
     }
     
+    /**
+     * Test for steward creation.
+     * @throws JPAException 
+     */
     @Test
     public void testCreateSteward() throws JPAException {
         StewardTO stewTo = newStewardTO("Elaine","Dickinson");
@@ -42,12 +70,17 @@ public class StewardServiceImplTest extends AbstractServiceTest {
     
     /**
      * Attempts to update Null Steward. Good luck with that!
+     * @throws cz.muni.fi.pa165.airportmanager.backend.daos.impl.JPAException
      */
     @Test (expected=DataAccessException.class)
     public void testUpdateNull() throws JPAException {
         service.updateSteward(null);
     }
     
+    /**
+     * Test for steward updating.
+     * @throws JPAException 
+     */
     @Test
     public void testUpdateSteward() throws JPAException {
         StewardTO stewTo = newStewardTO("Elaine","Dickinson");
@@ -59,13 +92,18 @@ public class StewardServiceImplTest extends AbstractServiceTest {
     }
     
     /**
-     * Attempts to remove Null Steward. Good luck with that!
+     * Attempts to remove Null Steward.
+     * @throws cz.muni.fi.pa165.airportmanager.backend.daos.impl.JPAException
      */
     @Test (expected=DataAccessException.class)
     public void testRemoveNull() throws JPAException {
         service.removeSteward(null);
     }
     
+    /**
+     * Test for removing steward.
+     * @throws JPAException
+     */
     @Test
     public void testRemoveSteward() throws JPAException {
         StewardTO stewTo = newStewardTO("Elaine","Dickinson");
@@ -76,22 +114,26 @@ public class StewardServiceImplTest extends AbstractServiceTest {
     }
     
     /**
-     * Attempts to find Null Steward. Good luck with that!
+     * Attempts to find Null Steward.
+     * @throws cz.muni.fi.pa165.airportmanager.backend.daos.impl.JPAException
      */
     @Test (expected=DataAccessException.class)
     public void testFindNull() throws JPAException {
         service.findSteward(null);
     }
     
-    //should work
+    /**
+     * Test for fidning steward.
+     * @throws JPAException 
+     */
     @Test
     public void testFindSteward() throws JPAException {
-        StewardTO expected = newStewardTO("Elaine","Dickinson");
+        StewardTO expected = Mockito.mock(StewardTO.class);
         try {
-            expected.setId(new Long(1));
+            
             service.createSteward(expected);
-            StewardTO actual = service.findSteward(expected.getId());
-            verify(stewDAO).getSteward(expected.getId());
+            StewardTO actual = service.findSteward(Mockito.mock(Long.class));
+            verify(stewDAO).getSteward(Mockito.mock(Long.class));
             assertEquals(expected, actual);
             assertDeepEquals(expected, actual);
         } catch (DataAccessException ex) {
@@ -101,12 +143,19 @@ public class StewardServiceImplTest extends AbstractServiceTest {
     
     @Test
     public void testFindAllStewards() throws JPAException {
-        service.findAllStewards();
+        StewardTO stew = newStewardTO("Elaine","Dickinson");
+        List<StewardTO> expected = new ArrayList();
+        expected.add(stew);
+        service.createSteward(stew);
+        List<StewardTO> actual = service.findAllStewards();
         verify(stewDAO).getAllStewards();
+        assertEquals(expected, actual);
+        
     }
     
     /**
-     * Attempts to retrive all Null Steward flights. Good luck with that!
+     * Attempts to retrive all Null Steward flights.
+     * @throws cz.muni.fi.pa165.airportmanager.backend.daos.impl.JPAException
      */
     @Test (expected=DataAccessException.class)
     public void testGetAllNullFlights() throws JPAException {
