2dab12766fe49167956064494087a8b6828c9f52
chorke <jura.durani@gamil.com>
2013-11-20 12:22:02 +0100

    steward - jsp, bean

---
+++ AirportManager/AirportManager-web/src/main/java/cz/muni/fi/pa165/airportmanager/web/beans/StewardsActionBean.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.airportmanager.web.beans;
+
+import net.sourceforge.stripes.action.UrlBinding;
+
+/**
+ *
+ * @author Chorke
+ */
+@UrlBinding("/stewards/{$event}/{steward.id}")
+public class StewardsActionBean extends BaseActionBean{
+    
+}
