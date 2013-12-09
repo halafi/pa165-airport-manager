d59b571329d311c7e6b144c8ebcbbe0db70b0a7e
Chorke <Chorke@147.251.232.15>
2013-12-09 11:02:42 +0100

    destination rest server

--- AirportManager/AirportManager-web/src/main/java/cz/muni/fi/pa165/airportmanager/web/rest/server/DestinationRestApi.java
+++ AirportManager/AirportManager-web/src/main/java/cz/muni/fi/pa165/airportmanager/web/rest/server/DestinationRestApi.java
@@ -4,18 +4,25 @@
  */
 package cz.muni.fi.pa165.airportmanager.web.rest.server;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import cz.muni.fi.pa165.airportmanager.services.DestinationService;
 import cz.muni.fi.pa165.airportmanager.transferobjects.DestinationTO;
+import cz.muni.fi.pa165.airportmanager.transferobjects.FlightTO;
 import java.util.List;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
-import org.hibernate.exception.DataException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.dao.DataAccessException;
@@ -30,7 +37,10 @@ public class DestinationRestApi {
     private static final ApplicationContext appConfig = 
             new ClassPathXmlApplicationContext("applicationContext.xml");
     
+    @Autowired
     private DestinationService destService;
+    
+    private ObjectMapper mapper = new ObjectMapper();
 
     public DestinationRestApi() {
         destService = appConfig.getBean(DestinationService.class);
@@ -38,25 +48,59 @@ public class DestinationRestApi {
     }
     
     @GET
-    public Response getAllDestinations(){
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getAllDestinations(){
         try{
-             return Response.ok(destService.getAllDestinations()).build();
-        } catch (DataException ex){
-            return Response.status(Status.NOT_FOUND).entity(ex).build();
-        } catch (Throwable ex){
-            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex).build();
+            return mapper.writerWithType(new TypeReference<List<DestinationTO>>() {})
+                    .writeValueAsString(destService.getAllDestinations());
+        } catch (DataAccessException ex){
+            throw new WebApplicationException(ex, Status.SERVICE_UNAVAILABLE);
+        } catch (Exception ex){
+            throw new WebApplicationException(ex, Status.INTERNAL_SERVER_ERROR);
         }
     }
     
     @GET
     @Path("/{id}")
-    public Response getAllDestinations(@PathParam("id") Long id){
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getDestination(@PathParam("id") Long id){
         try{
-             return Response.ok(destService.getDestination(id)).build();
-        } catch (DataException ex){
-            return Response.status(Status.NOT_FOUND).entity(ex).build();
+             return mapper.writerWithType(new TypeReference<DestinationTO>() {})
+                    .writeValueAsString(destService.getDestination(id));
+        } catch (DataAccessException ex){
+            throw new WebApplicationException(ex, Status.SERVICE_UNAVAILABLE);
         } catch (Throwable ex){
-            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex).build();
+            throw new WebApplicationException(ex, Status.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @GET
+    @Path("/{id}/incoming")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getIncomingFlights(@PathParam("id") Long id){
+        try{
+            DestinationTO des = destService.getDestination(id);
+             return mapper.writerWithType(new TypeReference<List<FlightTO>>() {})
+                    .writeValueAsString(destService.getAllIncomingFlights(des));
+        } catch (DataAccessException ex){
+            throw new WebApplicationException(ex, Status.SERVICE_UNAVAILABLE);
+        } catch (Throwable ex){
+            throw new WebApplicationException(ex, Status.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @GET
+    @Path("/{id}/outcoming")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getOutcomintFlights(@PathParam("id") Long id){
+        try{
+            DestinationTO des = destService.getDestination(id);
+             return mapper.writerWithType(new TypeReference<List<FlightTO>>() {})
+                    .writeValueAsString(destService.getAllOutcomingFlights(des));
+        } catch (DataAccessException ex){
+            throw new WebApplicationException(ex, Status.SERVICE_UNAVAILABLE);
+        } catch (Throwable ex){
+            throw new WebApplicationException(ex, Status.INTERNAL_SERVER_ERROR);
         }
     }
     
@@ -66,11 +110,43 @@ public class DestinationRestApi {
         try{
             DestinationTO des = destService.getDestination(id);
             destService.removeDestination(des);
-            return Response.ok().build();
-        } catch (DataException ex){
-            return Response.status(Status.NOT_FOUND).entity(ex).build();
+            return Response.status(Status.NO_CONTENT).build();
+        } catch (DataAccessException ex){
+            throw new WebApplicationException(ex, Status.SERVICE_UNAVAILABLE);
         } catch (Throwable ex){
-            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex).build();
+            throw new WebApplicationException(ex, Status.INTERNAL_SERVER_ERROR);
         }
     }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createDestination(String destination){
+        try{
+            DestinationTO des = mapper
+                    .readValue(destination, new TypeReference<DestinationTO>() {});
+            destService.createDestination(des);
+            return Response.status(Status.CREATED).build();
+        } catch (DataAccessException ex){
+            throw new WebApplicationException(ex, Status.SERVICE_UNAVAILABLE);
+        } catch (Throwable ex){
+            throw new WebApplicationException(ex, Status.INTERNAL_SERVER_ERROR);
+        }
+    } 
+    
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/{id}")
+    public Response updateDestination(String destination, @PathParam("id") Long id){
+        try{
+            DestinationTO des = mapper
+                    .readValue(destination, new TypeReference<DestinationTO>() {});
+            des.setId(id);
+            destService.updateDestination(des);
+            return Response.status(Status.OK).build();
+        } catch (DataAccessException ex){
+            throw new WebApplicationException(ex, Status.SERVICE_UNAVAILABLE);
+        } catch (Throwable ex){
+            throw new WebApplicationException(ex, Status.INTERNAL_SERVER_ERROR);
+        }
+    } 
 }
